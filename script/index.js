591580a2e9b971639462d12b3f881532a255dc91
MD. TAIFUR RAHMAN JASIM <taif.jnu@gmail.com>
2026-03-03 17:58:52 +0600

    Lesson level added

---
+++ script/index.js
@@ -0,0 +1,40 @@
+console.log("I am JS");
+
+const loadLessons = () => {
+    fetch("https://openapi.programming-hero.com/api/levels/all")
+        .then(res => res.json())
+        .then((json) => displayLessons(json.data));
+};
+loadLessons();
+
+
+const loadLevelWord = (id) => {
+    const url = `https://openapi.programming-hero.com/api/level/${id}`;
+    console.log(url);
+    fetch(url)
+        .then(res => res.json())
+        .then((data) => console.log(data));
+}
+
+const displayLevelWords = (word) => {
+    console.log(word);
+}
+
+const displayLessons = (lessons) => {
+    const levelContainer = document.getElementById("level-container");
+    levelContainer.innerHTML = "";
+    for (let lesson of lessons) {
+        const btnDiv = document.createElement("div");
+        btnDiv.innerHTML = `
+        <button onclick="loadLevelWord(${lesson.level_no})" class="btn btn-outline btn-primary">
+        <i class="fa-solid fa-book-open"></i>
+       lesson-${lesson.level_no}</button>
+        `;
+        console.log(btnDiv);
+
+        levelContainer.append(btnDiv);
+    }
+
+
+}
+
